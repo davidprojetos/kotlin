a5410c64ab1fb434dc941ceb85434d745cefb730
davidprojetos <davidffsousaff@gmail.com>
2024-01-12 20:03:22 -0300

    add templetes

--- spring-boot/appkotlin/src/main/kotlin/com/example/appkotlin/controller/HelloController.kt
+++ spring-boot/appkotlin/src/main/kotlin/com/example/appkotlin/controller/HelloController.kt
@@ -1,10 +1,10 @@
 // src/main/kotlin/com/example/controllers/HomeController.kt
 
-package com.example.appkotlin.controllers
+package com.example.appkotlin.controller;
 
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class HelloController {
