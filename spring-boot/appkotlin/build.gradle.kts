d8fce2c7e8238d5d2af8380c44d8610533c3e239
davidprojetos <davidffsousaff@gmail.com>
2024-01-13 00:08:27 -0300

    refatorando

--- spring-boot/appkotlin/build.gradle.kts
+++ spring-boot/appkotlin/build.gradle.kts
@@ -12,6 +12,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -19,6 +20,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("com.h2database:h2") // ou outro banco de dados de sua escolha
+
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
