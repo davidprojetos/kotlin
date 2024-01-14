53f009e7a278c828ff129d572c17d8c344e30fb7
davidprojetos <davidffsousaff@gmail.com>
2024-01-14 17:11:22 -0300

    atualizacao de projetos

--- spring-boot/demo/src/main/java/com/example/demo/CrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrudRepository extends JpaRepository<Registro, Long> {
-    // Adicione métodos de consulta personalizados, se necessário
-}
